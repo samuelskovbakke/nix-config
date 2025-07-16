fe07d9e6a7b8efe8e952ef8786c55ce8002325e1
samuelskovbakke <samuel@skovbakke.dk>
2025-07-16 22:49:49 +0200

    maybe git credential stuff works now

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,12 +1,10 @@
-{ pkgs, ... }:{
+{ ... }:{
   programs.git = {
     enable = true;
     userName = "samuelskovbakke";
     userEmail = "samuel@skovbakke.dk";
     extraConfig = {
-      credential.helper = "${
-          pkgs.git.override { withLibsecret = true; }
-        }/bin/git-credential-libsecret";
+      credential.helper = "oauth";
     };
   };
 }
