19b3f36cc8c56d894161529809ce18e69c61b173
samuelskovbakke <samuel@skovbakke.dk>
2025-07-20 20:10:11 +0200

    added more nix settings

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -1,9 +1,10 @@
-{
+{ pkgs, ...}: {
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings = {
     auto-optimise-store = true;
+    download-buffer-size = 524288000;
   };
 
   nix.gc = {
@@ -11,4 +12,9 @@
     dates = "daily";  # Or daily, monthly, etc.
     options = "--delete-older-than 7d";
   };
-}
+
+  environment.systemPackages = with pkgs; [
+    nix-index
+  ];
+
+  }
