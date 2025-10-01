f25bffe6409d7bc2d044f0a81057a016dbdbd880
samuelskovbakke <samuel@skovbakke.dk>
2025-10-01 14:10:49 +0200

    updated system and removed dns stuff

--- nixos/modules/net.nix
+++ nixos/modules/net.nix
@@ -2,25 +2,25 @@
   networking = {
     networkmanager = {
       enable = true;
-      dns = "systemd-resolved";
+      # dns = "systemd-resolved";
     };
     firewall = {
       enable = true;
       allowedTCPPorts = [22 80 443 5900];
     };
-    nameservers = ["1.1.1.1" "1.0.0.1"]; # Cloudflare DNS
+    # nameservers = ["1.1.1.1" "1.0.0.1"]; # Cloudflare DNS
   };
 
-  services.resolved = {
-    enable = true;
-    # fallbackDns = [];
-    dnsovertls = "true";
-    dnssec = "true";
-    domains = ["~."];
-    extraConfig = "Cache=yes";
-  };
+  # services.resolved = {
+  #   enable = true;
+  #   # fallbackDns = [];
+  #   dnsovertls = "true";
+  #   dnssec = "true";
+  #   domains = ["~."];
+  #   extraConfig = "Cache=yes";
+  # };
 
   # # These options are unnecessary when managing DNS ourselves
-  networking.useDHCP = false;
-  networking.dhcpcd.enable = false;
+  # networking.useDHCP = false;
+  # networking.dhcpcd.enable = false;
 }
