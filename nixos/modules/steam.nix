7288b4f3c9e687b5c9cb08b84d40a0b4b9a60219
samuelskovbakke <samuel@skovbakke.dk>
2026-05-14 01:21:55 +0200

    added gamescope and refactored

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,9 +1,15 @@
-{...}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraPackages = with pkgs; [gamescope];
+  };
+  hardware = {
+    steam-hardware.enable = true;
+    xone.enable = true;
+    xpadneo.enable = true;
   };
 
   hardware = {
