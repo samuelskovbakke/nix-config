71a00d35090ee62ab620a72abb21ff76dc6516c0
samuelskovbakke <samuel@skovbakke.dk>
2026-05-14 22:43:43 +0200

    added package to fix steam controller firmware update

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -4,8 +4,11 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    extraPackages = with pkgs; [gamescope];
+    extraPackages = with pkgs; [
+      hidapi
+    ];
   };
+
   hardware = {
     steam-hardware.enable = true;
     xone.enable = true;
