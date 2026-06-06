4396ace5e112915c9e921066eae9178dad073959
samuelskovbakke <samuel@skovbakke.dk>
2026-06-06 14:08:46 +0200

    added proton-ge and extest that enables steam controller 2 mouse support

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -3,13 +3,25 @@
     enable = true;
     package = pkgs.millennium-steam;
 
+    extest.enable = true; # Fixes steam controller mouse input
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
     extraPackages = with pkgs; [
       hidapi
     ];
+
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
+
+  /*
+  programs.gamescope = {
+    enable = true;
+  };
+  */
 
   hardware = {
     steam-hardware.enable = true;
