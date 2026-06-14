04c795091557317a42dd53dac75c574732dee66b
samuelskovbakke <samuel@skovbakke.dk>
2026-06-14 11:15:43 +0200

    added self-hosted search engine

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -1,5 +1,19 @@
 {
   services = {
     fwupd.enable = true;
+    searx = {
+      enable = true;
+      environmentFile = "/home/samuel/.searxng.env";
+      settings = {
+        general = {
+          debug = false;
+        };
+        server = {
+          bind_address = "127.0.0.1";
+          server.port = 8080;
+          secret_key = "$SEARX_SECRET_KEY";
+        };
+      };
+    };
   };
 }
