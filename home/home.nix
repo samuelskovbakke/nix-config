ed218d3ec9b5bce627af845ada068475a37f3f3c
samuelskovbakke <samuel@skovbakke.dk>
2026-07-30 17:52:59 +0200

    added gtk and qt theming
    instead of relying on separate dotfiles now theming is done in nixos

--- home/home.nix
+++ home/home.nix
@@ -2,6 +2,7 @@
   homeStateVersion,
   user,
   pkgs,
+  config,
   ...
 }: {
   imports = [
@@ -14,7 +15,9 @@
     homeDirectory = "/home/${user}";
     stateVersion = homeStateVersion;
   };
+
   news.display = "silent";
+
   home.pointerCursor = {
     enable = true;
     name = "BreezeX-RosePine-Linux";
@@ -23,5 +26,52 @@
     gtk.enable = true; # sets it for GTK apps
     x11.enable = true; # sets it for X11 apps (good fallback)
     hyprcursor.enable = false; # not needed for Niri
+  };
+
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "Catppuccin-Mocha-Standard-Sky-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["sky"];
+        size = "standard";
+        variant = "mocha";
+      };
+    };
+
+    iconTheme = {
+      name = "Flat-Remix-Blue-Dark";
+      package = pkgs.flat-remix-icon-theme;
+    };
+
+    font = {
+      name = "Noto Sans";
+      size = 10;
+    };
+
+    # keep legacy behavior: GTK4 uses the same theme as GTK3
+    gtk4.theme = config.gtk.theme;
+
+    # force dark preference for apps reading the old flag
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk3"; # makes Qt apps pull colors from your GTK theme
+    style.name = "adwaita-dark"; # fallback Qt widget style
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      monospace-font-name = "Hack 10";
+      color-scheme = "prefer-dark";
+    };
   };
 }
