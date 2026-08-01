ccdcf30b2ddbbb7c32c7170c77ae5c5d0316d001
samuelskovbakke <samuel@skovbakke.dk>
2026-08-02 01:29:19 +0200

    added system diagnostic tools

--- nixos/modules/extra-programs.nix
+++ nixos/modules/extra-programs.nix
@@ -2,5 +2,14 @@
   environment.systemPackages = with pkgs; [
     nix-prefetch-git
     typst
+
+    # System diagnostic packages
+    pciutils
+    usbutils
+    dmidecode
+    vulkan-tools
+    lm_sensors
+    lshw-gui
+    inxi
   ];
 }
