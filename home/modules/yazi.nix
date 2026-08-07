1eb7cbc84106fd192cb5ba59d63642dd9bf6ec17
samuelskovbakke <samuel@skovbakke.dk>
2026-08-07 12:31:48 +0200

    got yazi working through home-manager

--- home/modules/yazi.nix
+++ home/modules/yazi.nix
@@ -1,13 +1,20 @@
-{pkgs}: {
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
+    shellWrapperName = "y";
 
     flavors = {
-      inherit (pkgs.yaziPlugins) tokyonight;
+      tokyo-night = pkgs.fetchFromGitHub {
+        owner = "BennyOe";
+        repo = "tokyo-night.yazi";
+        rev = "main";
+        hash = "sha256-LArhRteD7OQRBguV1n13gb5jkl90sOxShkDzgEf3PA0=";
+      };
     };
+
     theme = {
       flavor = {
-        dark = "tokyonight";
+        dark = "tokyo-night";
       };
     };
   };
