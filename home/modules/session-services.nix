63c35fcbfa61172eef8d6f9a7eb1f6e12d0dd72d
samuelskovbakke <samuel@skovbakke.dk>
2026-08-07 22:03:32 +0200

    removed old kde polkit code

--- home/modules/session-services.nix
+++ home/modules/session-services.nix
@@ -1,24 +1,4 @@
 {pkgs, ...}: {
-  /*
-  systemd.user.services.polkit-kde-agent = {
-    Unit = {
-      Description = "polkit-kde-authentication-agent-1";
-      After = ["graphical-session.target"];
-      PartOf = ["graphical-session.target"];
-    };
-    Service = {
-      Type = "simple";
-      ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-      Restart = "on-failure";
-      RestartSec = 1;
-      TimeoutStopSec = 10;
-    };
-    Install.WantedBy = ["graphical-session.target"];
-  };
-
-  home.packages = [pkgs.kdePackages.qqc2-breeze-style];
-  */
-
   home.packages = with pkgs; [
     polkit_gnome
   ];
