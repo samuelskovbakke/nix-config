cb817858eb363b5cc630ed6ef52e5fd0f6820d0f
samuelskovbakke <samuel@skovbakke.dk>
2026-08-07 22:19:40 +0200

    moved theming to its own file

--- home/home.nix
+++ home/home.nix
@@ -1,14 +1,14 @@
 {
   homeStateVersion,
   user,
-  pkgs,
-  config,
+  lib,
+  host,
   ...
 }: {
-  imports = [
-    ./modules
-    ./home-packages.nix
-  ];
+  imports =
+    [./modules/common ./home-packages.nix]
+    ++ lib.optionals (host.class == "desktop") [./modules/desktop]
+    ++ lib.optionals (host.class == "server") [./modules/server];
 
   home = {
     username = user;
@@ -17,70 +17,4 @@
   };
 
   news.display = "silent";
-
-  home.pointerCursor = {
-    enable = true;
-    name = "BreezeX-RosePine-Linux";
-    package = pkgs.rose-pine-cursor;
-    size = 32;
-    gtk.enable = true; # sets it for GTK apps
-    x11.enable = true; # sets it for X11 apps (good fallback)
-    hyprcursor.enable = false; # not needed for Niri
-  };
-
-  home.packages = with pkgs; [
-    (catppuccin-gtk.override {
-      accents = ["sky"];
-      size = "standard";
-      variant = "mocha";
-    })
-  ];
-
-  gtk = {
-    enable = true;
-
-    theme = {
-      name = "catppuccin-mocha-sky-standard";
-      package = pkgs.catppuccin-gtk.override {
-        accents = ["sky"];
-        size = "standard";
-        variant = "mocha";
-      };
-    };
-
-    colorScheme = "dark";
-
-    iconTheme = {
-      name = "Flat-Remix-Blue-Dark";
-      package = pkgs.flat-remix-icon-theme;
-    };
-
-    font = {
-      name = "Noto Sans";
-      size = 10;
-    };
-
-    # keep legacy behavior: GTK4 uses the same theme as GTK3
-    gtk4.theme = config.gtk.theme;
-
-    # force dark preference for apps reading the old flag
-    gtk3.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = 1;
-    };
-  };
-  /*
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
-  */
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk3"; # makes Qt apps pull colors from your GTK theme
-    style.name = "kvantum"; # fallback Qt widget style
-  };
 }
