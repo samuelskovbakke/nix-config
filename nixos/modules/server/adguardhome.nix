47d93644c92e3870ce143aa1ada6a38a162216d5
samuelskovbakke <samuel@skovbakke.dk>
2026-08-09 12:03:46 +0200

    set up allowed ports for adguard home

--- nixos/modules/server/adguardhome.nix
+++ nixos/modules/server/adguardhome.nix
@@ -4,4 +4,9 @@
     openFirewall = true; # opens 53 (DNS) and 3000 (initial setup UI)
     # host defaults to 0.0.0.0, port 3000 for the web UI
   };
+
+  networking.firewall = {
+    allowedTCPPorts = [53];
+    allowedUDPPorts = [53];
+  };
 }
