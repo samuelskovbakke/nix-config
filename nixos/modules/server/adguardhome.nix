5dbfe6587ebd4f3a59025e3363d7587df87ac33c
samuelskovbakke <samuel@skovbakke.dk>
2026-08-09 11:22:00 +0200

    added adguard home as server service

---
+++ nixos/modules/server/adguardhome.nix
@@ -0,0 +1,7 @@
+{...}: {
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true; # opens 53 (DNS) and 3000 (initial setup UI)
+    # host defaults to 0.0.0.0, port 3000 for the web UI
+  };
+}
