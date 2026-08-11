6175542d2b8530f611d2b6f2f65dbe554caba3e0
samuelskovbakke <samuel@skovbakke.dk>
2026-08-11 23:09:23 +0200

    deleted packages.nix and split packages into separate files

---
+++ home/modules/common/packages/cli.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    bat
+    bc
+    btop
+    eza
+    fd
+    fetch # actively contributing; will replace fastfetch once mature
+    fzf
+    git
+    gum
+    jq
+    lazygit
+    lsof
+    ncdu
+    pay-respects
+    ripgrep
+    tldr
+    tree
+    unzip
+    wget
+    wol
+    yazi
+    zip
+    zoxide
+  ];
+}
