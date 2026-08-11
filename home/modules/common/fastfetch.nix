54e5b25ab8df446a2718e157326422b86e0a9cc1
samuelskovbakke <samuel@skovbakke.dk>
2026-08-11 23:08:45 +0200

    moved fastfetch package into this file

--- home/modules/common/fastfetch.nix
+++ home/modules/common/fastfetch.nix
@@ -1,7 +1,10 @@
-{...}: {
+{pkgs, ...}: {
   # Only the config actually in use is tracked here; config-compact.jsonc,
   # config-old.jsonc and config-v2.jsonc from the old dotfiles were unused
   # variants and were dropped.
+  home.packages = with pkgs; [
+    fastfetch # fallback until `fetch` has feature parity
+  ];
   xdg.configFile = {
     "fastfetch/config.jsonc".source = ./fastfetch/config.jsonc;
     "fastfetch/nixos-logo.png".source = ./fastfetch/nixos-logo.png;
