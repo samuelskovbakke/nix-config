bea3cab4507c2edd24043bde8d742edc7ba4f50e
samuelskovbakke <samuel@skovbakke.dk>
2026-08-26 16:22:14 +0200

    added specific default profile due to stylix needing it

--- home/modules/desktop/packages/browser.nix
+++ home/modules/desktop/packages/browser.nix
@@ -6,5 +6,9 @@
   programs.zen-browser = {
     enable = true;
     setAsDefaultBrowser = true;
+    profiles.default = {
+      isDefault = true;
+      path = "ygsl0z2v.Default Profile";
+    };
   };
 }
