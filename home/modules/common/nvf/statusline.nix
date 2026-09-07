c0c3065664102388d5d617c0501825488b53f712
samuelskovbakke <samuel@skovbakke.dk>
2026-09-07 17:31:56 +0200

    fixed rename of lualine sections

--- home/modules/common/nvf/statusline.nix
+++ home/modules/common/nvf/statusline.nix
@@ -1,21 +1,22 @@
 {...}: {
   programs.nvf.settings.vim.statusline.lualine = {
     enable = true;
-    activeSection.c = [
-      ''
-        {
-          "diff",
-          colored = false,
-          diff_color = {
-            -- Same color values as the general color option can be used here.
-            added    = 'DiffAdd',    -- Changes the diff's added color
-            modified = 'DiffChange', -- Changes the diff's modified color
-            removed  = 'DiffDelete', -- Changes the diff's removed color you
-          },
-          symbols = {added = " ", modified = " ", removed = " "}, -- Changes the diff symbols
-          separator = {right = ''}
-        }
-      ''
+    setupOpts.sections.lualine_c = [
+      {
+        __unkeyed-1 = "diff";
+        colored = false;
+        diff_color = {
+          added = "DiffAdd";
+          modified = "DiffChange";
+          removed = "DiffDelete";
+        };
+        symbols = {
+          added = " ";
+          modified = " ";
+          removed = " ";
+        };
+        separator.right = "";
+      }
     ];
   };
 }
