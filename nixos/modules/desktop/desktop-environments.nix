fb380551800e43ab2ca52d03fde76a2ae593f27f
samuelskovbakke <samuel@skovbakke.dk>
2026-09-11 13:49:32 +0200

    downgraded xwayland-satellite because of bug
    remember to change back to the newest version once a new release is
    made.

--- nixos/modules/desktop/desktop-environments.nix
+++ nixos/modules/desktop/desktop-environments.nix
@@ -61,7 +61,24 @@ with lib; {
     # Niri config
     (mkIf config.desktop.niri.enable {
       programs.niri.enable = true;
-      environment.systemPackages = with pkgs; [xwayland-satellite];
+      # environment.systemPackages = with pkgs; [xwayland-satellite];
+      # TODO: Remove below and re-enable the above package after fix is upstreamed
+      environment.systemPackages = [
+        (pkgs.xwayland-satellite.overrideAttrs (old: rec {
+          version = "0.8.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "Supreeeme";
+            repo = "xwayland-satellite";
+            tag = "v${version}";
+            hash = "sha256-BUE41HjLIGPjq3U8VXPjf8asH8GaMI7FYdgrIHKFMXA=";
+          };
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            inherit src;
+            name = "${old.pname}-${version}";
+            hash = "sha256-16L6gsvze+m7XCJlOA1lsPNELE3D364ef2FTdkh0rVY="; # will fail with correct hash, paste it in
+          };
+        }))
+      ];
     })
 
     # Hyprland config
